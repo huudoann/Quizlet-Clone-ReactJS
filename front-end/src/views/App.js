63ae2d750fc835e82d8a2b4a365621633f5d8e2d
Đức Chính Phạm <ducchinh.work@gmail.com>
2024-02-24 09:06:40 +0700

    update send data to server in login form

--- front-end/src/views/App.js
+++ front-end/src/views/App.js
@@ -13,37 +13,36 @@ import Auth2 from '../components/AuthForm/Auth2';
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
 
-  return (
-    <Router>
-      <div className="App">
-        {/* <Routes>
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
 
-          {/* 
-          Đang fake đã đăng nhập rồi bằng cách nhấn vào Quizlet khi ở trang trước khi đăng nhập
-          và ấn vào Home trên Nav của trang HomePage sau khi đăng nhập rồi để coi như đăng xuất 
-          
-          Sau khi sửa lại thì cần xóa phần Header của Sets và CreateSet
+                    Đang fake đã đăng nhập rồi bằng cách nhấn vào Quizlet khi ở trang trước khi đăng nhập
+                    và ấn vào Home trên Nav của trang HomePage sau khi đăng nhập rồi để coi như đăng xuất
 
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/lastest' element={<HomePage />} />
-          <Route exact path='/signup' element={<Auth />} />
-          <Route exact path='/login' element={<Auth2 />} />
-          <Route exact path='/lastest' element={<HomePage />} />
-          <Route exact path='/sets' element={<Sets />} />
-          <Route exact path='/create-set' element={<CreateSet />} />
-          <Route exact path='/flashcard' element={<Flashcard />} />
-          <Route exact path='/match' element={<Match />} />
-          <Route exact path='/learn' element={<Learn />} />
-        </Routes> */}
+                    Sau khi sửa lại thì cần xóa phần Header của Sets và CreateSet
 
-        {<Learn />}
-      </div>
-    </Router>
-  );
+                    <Route exact path='/' element={<Home />} />
+                    <Route exact path='/lastest' element={<HomePage />} />
+                    <Route exact path='/signup' element={<Auth />} />
+                    <Route exact path='/login' element={<Auth2 />} />
+                    <Route exact path='/lastest' element={<HomePage />} />
+                    <Route exact path='/sets' element={<Sets />} />
+                    <Route exact path='/create-set' element={<CreateSet />} />
+                    <Route exact path='/flashcard' element={<Flashcard />} />
+                    <Route exact path='/match' element={<Match />} />
+                    <Route exact path='/learn' element={<Learn />} />
+                </Routes>
+
+                {/* {<Learn />} */}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
