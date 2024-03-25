fba0bcaa4b4886c111c8d447923bc943ca4d39cc
Huu Doan <huudoan1204@gmail.com>
2024-03-25 13:49:13 +0700

    update Learn mode

--- front-end/src/components/Learn/Learn.js
+++ front-end/src/components/Learn/Learn.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Learn.scss';
 import { Close } from '@mui/icons-material';
 import DropDownMenu from '../Learn/DropDownMenu';
@@ -13,8 +13,11 @@ const Learn = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [inputDisabled, setInputDisabled] = useState(false);
   const [formHeight, setFormHeight] = useState('70vh');
+  const [showNextMessage, setShowNextMessage] = useState(false);
+  const [hintShown, setHintShown] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+  const refInput = useRef(null);
 
   useEffect(() => {
     if (!mounted) {
@@ -49,6 +52,25 @@ const Learn = () => {
     updateFormHeight();
   }, [flashcards, currentIndex, isCorrect]);
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (showNextMessage) {
+        setCurrentIndex(prevIndex => prevIndex + 1);
+        setUserInput('');
+        setIsCorrect(null);
+        setInputDisabled(false);
+        setFormHeight('70vh');
+        setShowNextMessage(false);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [showNextMessage]);
+
   const shuffleArray = (array) => {
     const newArray = [...array];
     for (let i = newArray.length - 1; i > 0; i--) {
@@ -63,17 +85,12 @@ const Learn = () => {
   };
 
   const handleInputChange = (e) => {
-    setUserInput(e.target.value);
+    const newValue = e.target.value;
+    setUserInput(newValue);
     setIsCorrect(null);
   };
-
   const handleSkip = () => {
-    // Chuyển sang thẻ kế tiếp nếu người dùng bấm nút Don't know
-    setCurrentIndex(prevIndex => prevIndex + 1);
-    setUserInput('');
-    setIsCorrect(null);
-    setInputDisabled(false); // Enable lại ô input khi chuyển thẻ
-    setFormHeight('70vh'); // Reset chiều cao của form
+    handleSubmit();
   };
 
   const handleSubmit = () => {
@@ -85,13 +102,20 @@ const Learn = () => {
         setUserInput('');
         setIsCorrect(null);
         setInputDisabled(false);
-        setFormHeight('70vh'); // Reset chiều cao của form
+        setFormHeight('70vh');
+        setShowNextMessage(false);
+        refInput.current.focus();
       }, 2000);
     } else {
       setIsCorrect(false);
       setInputDisabled(true);
-      setFormHeight('90vh');
+      setFormHeight('85vh');
+      setShowNextMessage(true);
     }
+  };
+
+  const handleShowHint = () => {
+    setHintShown(true);
   };
 
   return (
@@ -107,21 +131,13 @@ const Learn = () => {
             <div className='defi-box'>
               <p>Definition:</p>
               <span>{flashcards[currentIndex].back_text}</span>
-              <button className="hint-button">Show hint</button>
+              {hintShown ? (
+                <p className="hint-text">{flashcards[currentIndex].front_text.charAt(0) + flashcards[currentIndex].front_text.slice(1).replace(/[a-zA-Z]/g, '_')}</p>
+              ) : (
+                <button className="hint-button" onClick={handleShowHint}>Show hint</button>
+              )}
             </div>
             <div className='input-box'>
-              {isCorrect === false && (
-                <div className="incorrect-message">
-                  <p>No, sweat, you're still learning</p>
-                </div>
-              )}
-
-              {isCorrect === true && (
-                <div className="correct-message">
-                  <p>Amazing!</p>
-                </div>
-              )}
-
               <p>Your answer:</p>
               <input
                 type="text"
@@ -131,12 +147,29 @@ const Learn = () => {
                 onKeyUp={(e) => {
                   if (e.key === 'Enter') handleSubmit();
                 }}
-                disabled={inputDisabled} // Sử dụng biến state để kiểm soát trạng thái của ô input
+                disabled={inputDisabled}
+                ref={refInput}
               />
+              {isCorrect === false && (
+                <div className="incorrect-message">
+                  <p>Try next time!</p>
+                </div>
+              )}
+
+              {isCorrect === true && (
+                <div className="correct-message">
+                  <p>Amazing!</p>
+                </div>
+              )}
               {isCorrect === false && (
                 <div className="answer-box">
                   <p>Correct Answer:</p>
-                  <input type="text" value={flashcards[currentIndex].front_text} readOnly />
+                  <input
+                    type="text"
+                    value={flashcards[currentIndex].front_text}
+                    readOnly
+                    disabled={inputDisabled}
+                  />
                 </div>
               )}
               <div className='control-button'>
@@ -145,6 +178,18 @@ const Learn = () => {
               </div>
             </div>
           </>
+        )}
+        {showNextMessage && (
+          <div className="next-message">
+            <p>Press any key to move to the next question</p>
+          </div>
+        )}
+        {!flashcards[currentIndex] && (
+          <div className="summary">
+            <p>You've completed all cards!</p>
+            <p>Number of correct cards: {flashcards.filter(card => card.isCorrect).length}</p>
+            <button onClick={() => window.location.reload()}>Try Again</button>
+          </div>
         )}
       </div>
     </div>
