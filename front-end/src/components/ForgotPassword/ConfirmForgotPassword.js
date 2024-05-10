04fa0092b99ee2671b5aa14b46b2e37b4fe9047d
Huu Doan <huudoan1204@gmail.com>
2024-05-11 05:58:30 +0700

    update ui resetpassword page

--- front-end/src/components/ForgotPassword/ConfirmForgotPassword.js
+++ front-end/src/components/ForgotPassword/ConfirmForgotPassword.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Header from "../Header/Header";
 import "./ConfirmForgotPassword.scss";
 import toast, { Toaster } from "react-hot-toast";
 import { Button } from "@mui/material";
 
-const SettingPage = () => {
-    const [showChangePassword, setShowChangePassword] = useState(false);
+const ResetPasswordPage = () => {
     const [newPassword, setNewPassword] = useState("");
     const [confirmedPassword, setConfirmedPassword] = useState("");
+    const user_id = localStorage.getItem("user_id");
     const navigate = useNavigate();
 
     const handleChangePassword = async () => {
@@ -24,101 +23,93 @@ const SettingPage = () => {
             return;
         }
 
-        setShowChangePassword(false);
         let token = localStorage.getItem("token");
-        if (!token) {
-            throw new Error("Token không tồn tại trong localStorage");
-        } else {
-            try {
-                await axios.put(
-                    `http://localhost:8080/api/user/confirm-reset-password`,
-                    {
-                        password: newPassword,
+        // if (true) {
+
+        // } else {
+        try {
+            await axios.put(
+                `http://localhost:8080/api/user/change-password/${user_id}`,
+                {
+                    password: newPassword,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
                     },
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    }
-                );
-                window.location.reload();
-                toast.success("Đổi mật khẩu thành công");
-            } catch (error) {
-                toast.error("Lỗi khi đổi mật khẩu");
-            }
+                }
+            );
+            window.location.reload();
+            toast.success("Đổi mật khẩu thành công");
+        } catch (error) {
+            toast.error("Lỗi khi đổi mật khẩu");
         }
+        // }
     };
 
+    const handleCancelReset = () => {
+        navigate('/')
+    }
+
     return (
-        <div className="setting-page">
+        <div className="reset-password-page">
             <Toaster />
-            <div className="setting-page-container">
-                <span style={{ fontSize: "50px", fontWeight: "bold" }}>Đổi mật khẩu</span>
+            <div className="edit-password">
+                <div className="edit-password-box">
+                    <span style={{ fontSize: "50px", fontWeight: "bold" }}>Đổi mật khẩu</span>
+                    <div className="edit-password-input">
+                        <input
+                            type="password"
+                            placeholder="Mật khẩu mới"
+                            style={{
+                                fontSize: "20px",
+                                marginTop: "50px",
+                                width: "100%",
+                                height: "50px",
+                                borderRadius: "10px",
+                                paddingLeft: '.5rem'
+                            }}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Xác nhận mật khẩu mới"
+                            style={{
+                                fontSize: "20px",
+                                marginTop: "20px",
+                                width: "100%",
+                                height: "50px",
+                                borderRadius: "10px",
+                                paddingLeft: '.5rem'
 
-                <div className="user-infor-container">
-
-                    <div className="change-password-container">
-                        <div className="edit-password">
-                            <div className="edit-password-box">
-                                <div
-                                    className="edit-password-title"
-                                    style={{ fontSize: "30px", fontWeight: "bold" }}
-                                >
-                                    Đổi mật khẩu
-                                </div>
-                                <div className="edit-password-input">
-                                    <input
-                                        type="password"
-                                        placeholder="Mật khẩu mới"
-                                        style={{
-                                            fontSize: "20px",
-                                            marginTop: "50px",
-                                            width: "100%",
-                                            height: "50px",
-                                            borderRadius: "10px",
-                                        }}
-                                        onChange={(e) => setNewPassword(e.target.value)}
-                                    />
-                                    <input
-                                        type="password"
-                                        placeholder="Xác nhận mật khẩu mới"
-                                        style={{
-                                            fontSize: "20px",
-                                            marginTop: "20px",
-                                            width: "100%",
-                                            height: "50px",
-                                            borderRadius: "10px",
-                                        }}
-                                        onChange={(e) => setConfirmedPassword(e.target.value)}
-                                    />
-                                </div>
-                                <div className="edit-password-button-container">
-                                    <Button
-                                        onClick={() => setShowChangePassword(false)}
-                                        style={{
-                                            backgroundColor: "#303545",
-                                            color: "#fff",
-                                            fontSize: "16px",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        Hủy bỏ
-                                    </Button>
-                                    <Button
-                                        disabled={newPassword === "" || confirmedPassword === ""}
-                                        onClick={handleChangePassword}
-                                        style={{
-                                            backgroundColor: "#303545",
-                                            color: "#fff",
-                                            fontSize: "16px",
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        Lưu
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
+                            }}
+                            onChange={(e) => setConfirmedPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="edit-password-button-container">
+                        <Button
+                            onClick={handleCancelReset}
+                            style={{
+                                backgroundColor: "#303545",
+                                color: "#fff",
+                                fontSize: "16px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            Hủy bỏ
+                        </Button>
+                        <Button
+                            disabled={newPassword === "" || confirmedPassword === ""}
+                            onClick={handleChangePassword}
+                            style={{
+                                backgroundColor: "#303545",
+                                color: "#fff",
+                                fontSize: "16px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            Lưu
+                        </Button>
                     </div>
                 </div>
             </div>
@@ -126,4 +117,4 @@ const SettingPage = () => {
     );
 };
 
-export default SettingPage;
+export default ResetPasswordPage;
